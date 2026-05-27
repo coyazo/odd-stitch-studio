84717cb8c3f05d9999b815fe10743ae29b88ef4b
Carlos Coyazo <coyazo@gmail.com.com>
2026-05-26 21:48:31 -0700

    Fix layout children type

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,11 @@ export const metadata = {
   description: 'Odd Stitch Studio',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={ibmPlexMono.className}>
